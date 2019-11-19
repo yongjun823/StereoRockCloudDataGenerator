963b5d1cab3442f10608ecab5f8d8e80e6555e87
yongjun <yangyongjun823@gmail.com>
2019-11-19 18:25:33 +0900

    Rotate Rock

---
+++ Assets/RockBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehaviour : MonoBehaviour
+{
+    int cnt = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt++;
+        transform.rotation = Quaternion.Euler(0, cnt * 1, 0);
+    }
+}
