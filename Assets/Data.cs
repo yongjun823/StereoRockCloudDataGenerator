a0756abc8a96065b607a1253331642494775811b
yongjun <yangyongjun823@gmail.com>
2019-11-19 14:47:37 +0900

    init

---
+++ Assets/Data.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Data
+{
+    [System.Serializable]
+    public struct Point
+    {
+        public Vector3 position;
+        public uint color;
+    }
+}
