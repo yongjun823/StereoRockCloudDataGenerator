ca978f62b1d4d9ac5ab956d7b53899d64606fb88
yongjun <yangyongjun823@gmail.com>
2019-11-20 19:12:31 +0900

    Add point write logic

--- Assets/PointCamera.cs
+++ Assets/PointCamera.cs
@@ -1,38 +1,39 @@
 ﻿using Pcx;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class PointCamera : MonoBehaviour
 {
-    List<Data.Point> points;
     int cnt = 0;
-    // Start is called before the first frame update
-    void Start()
+
+    // Update is called once per frame
+    void Update()
     {
+        if (cnt > 10)
+        {
+            return;
+        }
+
         var pcr = GameObject.Find("Rock Cloud").GetComponent<PointCloudRenderer>();
         var computeBuffer = pcr.sourceData.computeBuffer;
         var pointList = new Data.Point[computeBuffer.count];
         computeBuffer.GetData(pointList);
 
-        var random = new Random();
+        var points = pointList.ToList()
+             .OrderBy(x => Random.Range(0, pointList.Length))
+             .Take(2048)
+             .ToList();
 
-        points = pointList.ToList()
-            .OrderBy(x => Random.Range(0, pointList.Length))
-            .Take(2048)
-            .ToList();
-    }
+        using (var file = new System.IO.StreamWriter($@"C:\sr\Rock1\{cnt}.txt"))
+        {
+            foreach (var point in points)
+            {
+                var position = point.position;
+                //var color = point.color;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-        //foreach(var point in points)
-        //{
-        //    var position = point.position;
-        //    var color = point.color;
-        //}
+                file.WriteLine($"{position.x}, {position.y}, {position.z}");
+            }
+        }
 
         cnt++;
     }
