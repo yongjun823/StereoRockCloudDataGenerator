20e882862bf75f65293d51ba21919ea75a5a0fa4
yongjun <yangyongjun823@gmail.com>
2019-11-27 19:55:56 +0900

    Add Camera Move Logic

--- Assets/CameraNewBehaviour.cs
+++ Assets/CameraNewBehaviour.cs
@@ -2,9 +2,42 @@
 
 public class CameraNewBehaviour : MonoBehaviour
 {
+    int angle_cnt = 1;
+    int position_cnt = 0;
+
+    void InitState(Transform transform)
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.position = new Vector3(0, 0, -2);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.up, 30 * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.up, 50 * Time.deltaTime);
+        
+        var angle = transform.rotation.eulerAngles.y;
+
+        //Debug.Log($"{cnt}, {angle / Mathf.Deg2Rad}");
+        if (angle > 350)
+        {
+            if (angle_cnt < 5)
+            {    
+                angle_cnt++;
+            }
+            else
+            {
+                if (position_cnt < 5)
+                {
+                    position_cnt++;
+                    angle_cnt = 0;
+                }
+            }
+
+            InitState(transform);
+            transform.Translate(new Vector3(0, 0.1f * position_cnt, 0.1f * angle_cnt));
+        }
+
+        Debug.Log($"{angle} / {angle_cnt} / {position_cnt}");
     }
 }
